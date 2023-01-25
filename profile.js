ba12b06c8aa9d213d6f417ffc91202b224f05bdd
joynto1 <joynto92@gmail.com>
2023-01-25 14:30:36 +0600

    ..

---
+++ profile.js
@@ -0,0 +1,10 @@
+////////////sidebar/////////////////
+function sidebar(){
+    var navbar = document.getElementById("navbar");
+   setTimeout(function(){
+    navbar.classList.toggle("show");
+   },
+  90
+   );
+  };
+  
